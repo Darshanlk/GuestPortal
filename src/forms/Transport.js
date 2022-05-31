c6c41c886ef43a5bc6708a6d00d2716c3d02a1f7
darshan <darshan.mistry@ezeetechnosys.com>
2022-05-31 12:17:56 +0530

    date and time picker

--- src/forms/Transport.js
+++ src/forms/Transport.js
@@ -9,59 +9,63 @@ import {
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-import {useFormik} from 'formik'
-import * as Yup from 'yup'
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-
-
-
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 
 function Transport() {
+  const [valueDate, setValueDate] = React.useState(new Date());
+  const [valueTime, setValueTime] = React.useState(new Date());
 
-
-
- const [value, setValue] = React.useState(new Date());
-
-
-
-// for dropdown
+  // for dropdown
   const StyledAccordion = styled(Accordion)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     color: theme.palette.text.secondary,
   }));
 
+  //validation  formik  and Yup
 
-//validation  formik  and Yup
-
-const formik1 = useFormik({
-
-    initialValues:{
-       pickupBy:"",
-       desciption:"",
-       transportNameNumber:"",
-       date:"",
-       time:""
-       
-        
+  const formik1 = useFormik({
+    initialValues: {
+      pickupBy1: "",
+      desciption1: "",
+      transportNameNumber1: "",
+      date1: new Date(),
+      time1: new Date(),
     },
-    validationSchema:Yup.object({
-        desciption:Yup.string().required("Please enter require transport info. "),
-        date:""
-    })
+    validationSchema: Yup.object({
+      desciption1: Yup.string().required("Please enter Your Description. "),
+      transportNameNumber1: Yup.string().required(
+        "Please enter required transport info."
+      ),
+    }),
+    onSubmit: (values) => {
+      console.log("formSubmitted", values);
+    },
+  });
 
-})
-
-
-
-
-
-
-
-
-
+  const formik2 = useFormik({
+    initialValues: {
+      pickupBy2: "",
+      desciption2: "",
+      transportNameNumber2: "",
+      date2: new Date(),
+      time2: new Date(),
+    },
+    validationSchema: Yup.object({
+      desciption2: Yup.string().required("Please enter Your Description. "),
+      transportNameNumber2: Yup.string().required(
+        "Please enter required transport info."
+      ),
+    }),
+    onSubmit: (values) => {
+      console.log("formSubmitted", values);
+    },
+  });
 
   return (
     <>
@@ -75,6 +79,7 @@ const formik1 = useFormik({
             <Typography variant="h6">Require Pickup?</Typography>
             <Box
               component="form"
+              onSubmit={formik1.handleSubmit}
               sx={{
                 display: "flex",
                 flexDirection: "column",
@@ -90,28 +95,43 @@ const formik1 = useFormik({
                 <AccordionDetails>Other</AccordionDetails>
               </StyledAccordion>
 
-              <TextField label="Description" type="text" name="desciption" error="" />
+              <TextField
+                label="Description"
+                type="text"
+                name="desciption1"
+                onChange={formik1.handleChange}
+                error={formik1.touched.desciption1 && Boolean(formik1.errors.desciption1)}
+                onBlur={formik1.handleBlur}
+                helperText={formik1.touched.desciption1 && formik1.errors.desciption1}
+              />
               <TextField
                 label="Transport Name and number"
                 type="text"
-                name="transportNameNumber"
+                name="transportNameNumber1"
+                onChange={formik1.handleChange}
               />
-               <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <DatePicker
-          disableFuture
-          label="Responsive"
-          openTo="year"
-          views={['year', 'month', 'day']}
-          value={value}
-          onChange={(newValue) => {
-            setValue(newValue);
-          }}
-          renderInput={(params) => <TextField {...params} />}
-        />
-        </LocalizationProvider>
-              <TextField label="Time" type="time" name="time" />
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  disableFuture
+                  name="date1"
+                  label="Date"
+                  openTo="year"
+                  views={["year", "month", "day"]}
+                  onChange={formik1.handleChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+                <TimePicker
+                  name="time1"
+                  label="Time"
+                  onChange={formik1.handleChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+              <Button variant="contained" type="submit" >
+              Submit
+            </Button>
             </Box>
-            <Button variant="contained">Submit</Button>
+           
           </Card>
         </Grid>
 
@@ -120,6 +140,7 @@ const formik1 = useFormik({
             <Typography variant="h6">Require Dropoff?</Typography>
             <Box
               component="form"
+              onSubmit={formik2.handleSubmit}
               sx={{
                 display: "flex",
                 flexDirection: "column",
@@ -139,12 +160,29 @@ const formik1 = useFormik({
               <TextField
                 label="Transport Name and number"
                 type="text"
-                name=""
+                name="description2"
               />
-              <TextField label="Date" type="date" name="" />
-              <TextField label="Time" type="time" name="" />
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  disableFuture
+                  label="Date"
+                  openTo="year"
+                  views={["year", "month", "day"]}
+                  onChange={formik1.onChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+                <TimePicker
+                  label="Time"
+                  value={valueTime}
+                  onChange={setValueTime}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+                <Button variant="contained" type="sumbit">
+              Submit
+            </Button>
+              </LocalizationProvider>
             </Box>
-            <Button variant="contained">Submit</Button>
+            
           </Card>
         </Grid>
       </Grid>
